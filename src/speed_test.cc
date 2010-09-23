67bfcb3c8bc102bbc9a759402fd4e763ca159338
Tom Marsh <t.r.marsh@warwick.ac.uk>
2010-09-23 22:29:24 +0100

    Speed test program added

---
+++ src/speed_test.cc
@@ -0,0 +1,23 @@
+#include <cstdlib>
+#include <iostream>
+#include "trm_subs.h"
+#include "trm_vec3.h"
+#include "trm_roche.h"
+
+int main(){
+
+    Subs::Vec3 r;
+    double q = 0.2;
+    double iangle = 87.;
+    int N = 200000;
+    Subs::INT4 seed = -12345;
+    double ffac = 0.9, ingress, egress;
+    for(int i=0; i<N; i++){
+	r.x() = -0.5 + Subs::ran1(seed);
+	r.y() = -0.5 + Subs::ran1(seed);
+	r.z() = -0.5 + Subs::ran1(seed);
+	bool ecl = Roche::ingress_egress(q, ffac, iangle, r,  ingress, egress, 3.e-8, Roche::SECONDARY);
+	std::cout << r << " " << ecl << " " << ingress << " " << egress << std::endl;
+    }
+    
+}
