0e911a30acd5221351c432cb3b0f9ada0ee56386
Tom Marsh <t.r.marsh@warwick.ac.uk>
2016-11-16 17:35:38 +0000

    Tightened up tolerances on the Lagrangian point routines
    Removed little fudge from face.cc. Hope I don't regret doing so.

--- src/xl12.cc
+++ src/xl12.cc
@@ -44,18 +44,17 @@ it returns with the (distance primary --> inner lagrangian)/separation
 double Roche::xl12(double q, double spin){
 
     const int NMAX   = 1000;
-    const double EPS = 1.e-8;
- 
+    const double EPS = 1.e-12;
+
     double ssq = spin*spin;
     double x, xold, f, df, mu;
     double a1, a2, a3, a4, a5, a6, d1, d2, d3, d4, d5;
     int n;
-    
+
     if(q <= 0.)
-	throw Roche_Error("Roche::xl12(double, double): q = " + Subs::str(q) + " <= 0.");
-    
+        throw Roche_Error("Roche::xl12(double, double): q = " + Subs::str(q) + " <= 0.");
+
     // Set poly coefficients
-    
     mu = q/(1.+q);
     a1 = -1.+mu;
     d1 = 1.*(a2 =  2.-2.*mu);
@@ -63,18 +62,18 @@ double Roche::xl12(double q, double spin){
     d3 = 3.*(a4 =  3.*ssq+2.*mu-2.);
     d4 = 4.*(a5 = 1.-mu-3.*ssq);
     d5 = 5.*(a6 =  ssq);
-    
+
     n = 0;
     xold = 0.;
     x    = 1./(1.+ q);
-    while(n < NMAX && fabs(x-xold) > EPS*fabs(x)){    
-	xold = x;
-	f    = x*(x*(x*(x*(x*a6+a5)+a4)+a3)+a2)+a1;
-	df   = x*(x*(x*(x*d5+d4)+d3)+d2)+d1;
-	x    = x - f/df;
-	n++;
+    while(n < NMAX && fabs(x-xold) > EPS*fabs(x)){
+        xold = x;
+        f    = x*(x*(x*(x*(x*a6+a5)+a4)+a3)+a2)+a1;
+        df   = x*(x*(x*(x*d5+d4)+d3)+d2)+d1;
+        x   -= f/df;
+        n++;
     }
     if(n == NMAX)
-	throw Roche_Error("Roche::xl12(double,double): exceeded maximum iterations");
+        throw Roche_Error("Roche::xl12(double,double): exceeded maximum iterations");
     return x;
 }
--- src/xl2.cc
+++ src/xl2.cc
@@ -35,17 +35,16 @@ where q = M2/M1 is the mass ratio and it returns with the
 double Roche::xl2(double q){
 
   const int NMAX = 1000;
-  const double EPS = 1.e-8;
+  const double EPS = 1.e-12;
 
   double x, xold, f, df, mu;
   double a1, a2, a3, a4, a5, a6, d1, d2, d3, d4, d5;
   int n;
 
   if(q <= 0.)
-    throw Roche_Error("Roche::xl2(double): q = " + Subs::str(q) + " <= 0.");
+      throw Roche_Error("Roche::xl2(double): q = " + Subs::str(q) + " <= 0.");
 
   // Set poly coefficients
-
   mu = q/(1.+q);
   a1 = -1.+mu;
   d1 = 1.*(a2 =  2.-2.*mu);
@@ -57,14 +56,14 @@ double Roche::xl2(double q){
   n    = 0;
   xold = 0.;
   x    = 1.5;
-  while(n < NMAX && fabs(x-xold) > EPS*fabs(x)){    
-    xold = x;
-    f    = x*(x*(x*(x*(x*a6+a5)+a4)+a3)+a2)+a1;
-    df   = x*(x*(x*(x*d5+d4)+d3)+d2)+d1;
-    x   += -f/df;
-    n++;
+  while(n < NMAX && fabs(x-xold) > EPS*fabs(x)){
+      xold = x;
+      f    = x*(x*(x*(x*(x*a6+a5)+a4)+a3)+a2)+a1;
+      df   = x*(x*(x*(x*d5+d4)+d3)+d2)+d1;
+      x   -= f/df;
+      n++;
   }
   if(n == NMAX)
-    throw Roche_Error("Roche::xl2(double): exceeded maximum iterations");
+      throw Roche_Error("Roche::xl2(double): exceeded maximum iterations");
   return x;
 }
